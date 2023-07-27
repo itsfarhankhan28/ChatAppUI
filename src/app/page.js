4facdf5a2407fb7404efd4638f24bc986b823a5b
itsfarhankhan28 <famannat@gmail.com>
2023-07-27 15:13:39 +0530

    feat(page): added new content

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
     <div className='flex justify-center items-center h-screen'>
-      <div className='border border-black'>
-        <h1>Homepage</h1>
+      <div className=''>
+        <div>
+          <img src={'./IMG/MOkx logo 1.png'}/>
+        </div>
+        <div className='fixed bottom-[10%] md:w-[12%] xxsm:w-[50%] text-center flex justify-center items-center'>
+          <h1 className='font-semibold text-typo-color'>
+            Back to Vedas 🕉️
+          </h1>
+        </div>
       </div>
     </div>
     </>
