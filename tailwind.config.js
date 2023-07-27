010e84a16e4036b754cfad1bf0e5f64e977ac66a
itsfarhankhan28 <famannat@gmail.com>
2023-07-27 15:13:06 +0530

    feat(tailwind): added new screens value

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xxsm: '350px',
+      xsm: '425px',
+      mobile: '425px',
+      sm: '640px',
+      tablet: '640px',
+      md: '768px',
+      lg: '1024px',
+      laptop: '1024px',
+      xl: '1440px',
+      desktop: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
