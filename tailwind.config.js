589197ea688fbcef7ea8ca97fb0d04289fefec8e
itsfarhankhan28 <famannat@gmail.com>
2023-07-28 12:33:06 +0530

    feat(tailwind): added bg-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'bg-image': "url('/IMG/secondpagebg.png')"
       },
       colors:{
         'bg-color':'#FFFFFF',
